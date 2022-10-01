4cfd3d20bf1abdcc7074fc6819955b76fc54a7d8
MatheusSan <matheus08santos@hotmail.com>
2022-09-30 23:25:16 -0300

    feat: criação da estrutura do projeto

---
+++ js/usuario.js
@@ -0,0 +1,7 @@
+var btnLogin = document.querySelector("#btnLogin");
+
+btnLogin.addEventListener("click", function (event) {
+  event.preventDefault();
+  alert("Loguei nessa caralha");
+  //TODO: Criar função de validação e login
+});
