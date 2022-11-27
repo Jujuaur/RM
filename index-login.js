732650db4c7650e8e1d7ea8974056f2ee6f5100c
MatheusSan <matheus08santos@hotmail.com>
2022-11-27 19:49:37 -0300

    Refact: assets de validação de senha e cpf

--- index-login.js
+++ index-login.js
@@ -1,29 +1,9 @@
 import { login } from "./js/firebase/fire-users.js";
 import { getSession } from "./js/session/index.js";
+import { validarLogin, validarSenha } from "./js/assets/index.js";
 const formLogin = document.querySelector("#formLogin");
 const cpfInput = document.querySelector("#cpf"); // cria uma variável com o valor do input que tem id #cpf
 const senhaInput = document.querySelector("#senha"); // cria uma variável com o valor do input que tem id #senha
-
-function validarLogin(cpf, senha) {
-  cpf = cpf.replace(/[^\d]/g, "");
-  var Soma;
-  var Resto;
-  Soma = 0;
-  if (cpf == "00000000000") return false;
-  for (let i = 1; i <= 9; i++)
-    Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-  Resto = (Soma * 10) % 11;
-  if (Resto == 10 || Resto == 11) Resto = 0;
-  if (Resto != parseInt(cpf.substring(9, 10))) return false;
-  Soma = 0;
-  for (let i = 1; i <= 10; i++)
-    Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-  Resto = (Soma * 10) % 11;
-  if (Resto == 10 || Resto == 11) Resto = 0;
-  if (Resto != parseInt(cpf.substring(10, 11))) return false;
-  if (senha.length < 8) return false;
-  return true;
-}
 
 window.formataCPF = function formataCPF(cpf) {
   //retira os caracteres indesejados...
@@ -36,7 +16,7 @@ window.formataCPF = function formataCPF(cpf) {
 formLogin.addEventListener("submit", function (e) {
   e.preventDefault(); // Impede o comportamento padrão do submit do formulário (recarregar a página)
 
-  if (validarLogin(cpfInput.value, senhaInput.value)) {
+  if (validarLogin(cpfInput.value) && validarSenha(senhaInput.value)) {
     login(cpfInput.value, senhaInput.value);
   } else {
     alert("Tem algo de errado com seu CPF ou senha");
