2bc92fd02ce82451519f20ceba2550e7b845cce7
juju <julia.aureliano12@gmail.com>
2022-12-06 23:35:23 -0300

    kkknao aguento mais

--- cadastro/index.js
+++ cadastro/index.js
@@ -1,11 +1,19 @@
-export { login } from "../js/firebase/fire-users";
-export { getSession } from "../js/session";
-export { validarLogin, validarSenha } from "../js/assets";
-const formLogin = document.querySelector("#formLogin");
-const cpfInput = document.querySelector("#cpf"); 
-const senhaInput = document.querySelector("#senha"); 
+import { validarLogin } from "../js/assets";
+import { validarSenha } from "../js/assets";
+const formlogin = document.querySelector ("#formlogin");
+const cpfInput = document.querySelector ("#cpf");
+const senhaInput = document.querySelector ("#senha");
 
 window.formataCPF = function formataCPF(cpf) {
-    cpf = cpf.replace(/[^\d]/g, "");
-    cpfInput.value = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+    cpf = cpf.replace (/[^\d]/g, "");
+    cpfInput.value = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+}
+formlogin.addEventListener ("submit" , function (e) {
+    e.preventDefault();
+    if (validarLogin(cpfInput.value) && validarSenha (senhaInput.value)) {
+        login
+    } else {
+        
+    }
+
 }
